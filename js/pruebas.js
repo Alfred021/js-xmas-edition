9a26d98f3663d7c3161f15158597018a890558f1
Alfred021 <alfredomedina10@hotmail.com>
2019-12-15 14:44:01 -0300

    Tarea clase 7
    subiendo tarea 7 para revision

--- js/pruebas.js
+++ js/pruebas.js
@@ -1,15 +1,65 @@
 function probarValidarNombre() {
   console.assert(
       validarNombre('') === 'Este campo debe tener al menos 1 caracter',
-      'Validar nombre no validó que el nombre no sea vacío',
-  );
+      'validarNombre no validó que el nombre no sea vacío'
+  );    
 
   console.assert(
       validarNombre(
           '111111111111111111111111111111111111111111111111111111111111111111111111111111111111111') ===
       'Este campo debe tener menos de 50 caracteres',
-      'Validar nombre no validó que el nombre sea menor a 50 caracteres',
+      'validarNombre no validó que el nombre sea menor a 50 caracteres'
+  );
+
+    console.assert (
+        validarNombre('89797489127491') === 'El nombre solo debe tener letras',
+        'validarNombre no valido que el nombre solo contenga letras'
+    );
+
+
+  console.assert(
+      validarNombre('Alfredo') === '',
+      'validarNombre fallo con un nombre valido'
   );
 }
 
 probarValidarNombre();
+
+function probarValidarCiudad() {
+    console.assert(
+        validarCiudad("") === 'Debes seleccionar una ciudad', 
+        'validarCiudad no valido que la ciudad este seleccionada'
+    );
+
+    console.assert(
+        validarCiudad("Buenos Aires") === '',
+        'validarCiudad no valido que el campo ciudad este vacio'
+    );
+}
+
+probarValidarCiudad();
+
+function probarvalidarDescripcionRegalo () {
+
+    console.assert (
+        validarDescripcionRegalo ('') === 'El campo de descripcion no debe estar vacio',
+        'validarDescripcionRegalo no valido que el campo de descripcion este vacio'
+    );
+
+    console.assert (
+        validarDescripcionRegalo ('koojonmjfoiajiofjaiofjaoifjaojfajfoiajfoijaofjafjaojfaojfoiajfoiajfojafjaofjaojfoajfoajfoajfaofjaojfoi') === 'El campo de descripcion es demasiado largo', 
+        'validarDescripcionRegalo no valido que el campo sea muy largo'
+    );
+
+    console.assert (
+        validarDescripcionRegalo (',.,.,.,,.') === 'El campo descripcion debe tener solo letras y numeros',
+        'validarDescripcionRegalo no valido que la descripcion solo tenga letras y numeros'
+    );
+
+    console.assert (
+        validarDescripcionRegalo("Me gusto el regalo") === '',
+        'validarDescripcionRegalo no funciono con una descripcion correcta'
+    );
+}
+
+probarvalidarDescripcionRegalo ();
