ed1e5abfadf9e69191a90568359ce0923dd07569
Alfred021 <alfredomedina10@hotmail.com>
2019-12-25 19:11:48 -0300

    Tarea clase 7
    tarea completa, con los errores correspondientes borrandose a medida que el usuario completa los campos correctamente

--- js/main.js
+++ js/main.js
@@ -1,16 +1,11 @@
-const form = document.querySelector('#carta-a-santa');
 
+const form = document.querySelector('#carta-a-santa');
 const nombre = form['nombre'].value;
 const ciudad = form['ciudad'].value;
 const comportamiento = form['comportamiento'].value;
 const descripcionRegalo = form['descripcion-regalo'].value;
 
-console.log(nombre);
-console.log(ciudad);
-console.log(comportamiento);
-console.log(descripcionRegalo);
-
-function validarNombre (nombre) {
+function validarNombre(nombre) {
     if (nombre.length === 0) {
         return 'Este campo debe tener por lo menos 1 caracter';
     }
@@ -26,7 +21,7 @@ function validarNombre (nombre) {
     return '';
 }
 
-function validarCiudad (ciudad) {
+function validarCiudad(ciudad) {
     if (ciudad === "") {
         return 'Debes seleccionar una ciudad';
     }
@@ -34,7 +29,7 @@ function validarCiudad (ciudad) {
     return '';
 }
 
-function validarDescripcionRegalo (descripcionRegalo) {
+function validarDescripcionRegalo(descripcionRegalo) {
     if (descripcionRegalo.length === 0) {
         return 'Este campo debe tener al menos un caracter'
     }
@@ -43,9 +38,74 @@ function validarDescripcionRegalo (descripcionRegalo) {
         return 'Este campo debe tener menos de 100 caracteres'
     }
 
-    if (!/^[A-z0-9]+$/i.test(descripcionRegalo)) {
+    if (!/^[A-z 0-9]+$/i.test(descripcionRegalo)) {
         return 'El campo descripcion debe tener solo letras y numeros'
     }
 
     return '';
 }
+
+function validarFormulario(event) {
+    const form = document.querySelector('#carta-a-santa');
+
+    const nombre = form['nombre'].value;
+    const ciudad = form['ciudad'].value;
+    const descripcionRegalo = form['descripcion-regalo'].value;
+
+    const errorNombre = validarNombre(nombre);
+    const errorCiudad = validarCiudad(ciudad);
+    const errorDescripcionRegalo = validarDescripcionRegalo(descripcionRegalo);
+
+    const errores = {
+        nombre: errorNombre,
+        ciudad: errorCiudad,
+        'descripcion-regalo': errorDescripcionRegalo,
+    };
+
+    const exito = manejarErrores(errores) === 0;
+
+    if (exito) {
+        form.className = "oculto";
+        document.querySelector('#exito').className = "";
+        redirectHtml();
+    }
+
+
+    event.preventDefault();
+}
+
+function redirectHtml() {
+    setTimeout(function() {
+        window.location.href = "wishlist.html";
+    }, 5000);
+
+}
+
+function manejarErrores(errores) {
+    const keys = Object.keys(errores);
+    const errors = document.querySelector('#errores');
+    let cantidadErrores = 0
+    while(errors.hasChildNodes()){
+        errors.removeChild(errors.firstChild);    
+    }
+
+    keys.forEach(function(key) {
+        const error = errores[key];
+
+        if (error) {
+            cantidadErrores++;
+            form[key].className = "error"
+            
+            const $error = document.createElement('li');
+            $error.innerText = error;
+            errors.appendChild($error);
+
+        } else {
+         form[key].className = ""   
+        }
+    
+    });
+    return cantidadErrores;
+}
+
+form.onsubmit = validarFormulario;
